20ebf87fe408714b82ac87a4413c7efae7ed170f
Nick Kinlen <nick_kinlen@hotmail.com>
2018-08-08 01:20:04 -0400

    Worked on fixing searchbar

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
     $('.menu-button').on("click", function(){
-      $('.menu').width("250px");
-    })
+      $('.menu').width("200px");
+    });
 
     $('.close-button').on("click", function() {
       $('.menu').width("0px");
-    })
+    });
+
+    $('.twitter-button').on("click", function() {
+      $('.twitter-menu').width("102px");
+      $('.twitter-menu').css("display", "block");
+    });
+
+    $(document).mouseup(function(e) {
+      var container = $(".twitter-menu");
+
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+        $(".twitter-menu").unbind( 'click', clickDocument );
+      }
+    });
+
+    $('.fa-arrow-circle-down').hide();
+
+    var waypoint = new Waypoint({
+      element: document.getElementById('main-h1'),
+      handler: function() {
+        $('.fa-arrow-circle-down').show();
+      },
+        offset: -100
+    });
+
+    $("a[href='#main-h1']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
+
+
 });
